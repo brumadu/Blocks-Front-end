059d4234c778b3a0e851d572d69337b37f89a794
brumadu <madureira12.bm@gmail.com>
2023-05-31 04:00:50 -0300

    Feature: Hover + Footer // Refactor: Clean css

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -1,8 +1,8 @@
 import { useRef, useCallback } from 'react'
-import Post from './Post'
+import Post from './Components/Post'
 import { useInfiniteQuery } from 'react-query'
 import { getPostsPage } from './api/axios'
-import logo from './logo.svg'
+import logo from './assets/logo.svg'
 
 const FrontPage = () => {
 
@@ -37,39 +37,42 @@ const FrontPage = () => {
     if (status === 'error') return <p className='center'>Erro: {error.message}</p>
 
     const content = data?.pages.map((pg, i) => {
-            if (pg.length === i + 1) {
-                return <Post ref={lastPostRef} key={pg.post.id} post={pg} />
-            }
-            return <Post key={pg.post.id} post={pg} />
+        if (pg.length === i + 1) {
+            return null
+        }
+        return null
     })
 
     return (
         <>
             <header className='fixed-top-bar'>
                 <div className='top-bar-content'>
-                            <p className='top-bar-text'>Não limite sua criatividade, junte-se a familia Blocks por apenas <b>BRL 19,99</b></p>
-                            <button className='premium-top-bar-button' type="button">Quero ser Premium</button>
+                    <p className='top-bar-text'>Não limite sua criatividade, junte-se a familia Blocks por apenas <b>BRL 19,99</b></p>
+                    <button className='premium-top-bar-button' type="button">Quero ser Premium</button>
                 </div>
-            </header>
-            <header className='logo-header'>
                 <div className='logo-content'>
-                                <img src={logo} alt="Blocks Logo"></img>
+                    <img src={logo} alt="Blocks Logo"></img>
                 </div>
-            <div className='logo-divider'></div>
+                <div className='logo-divider'></div>
             </header>
-            <header className='catalog-header'>
+            <div className='catalog-header'>
                 <div className='catalog-content'>
-                            <p className='catalog-text'>Catálogo<br/>
-                            <span className='catalog-box'></span>
-                            </p>
+                    <p className='catalog-text'>Catálogo<br />
+                        <span className='catalog-box'></span>
+                    </p>
                 </div>
-            <div className='catalog-divider'></div>
-            </header>
+                <   div className='catalog-divider'></div>
+            </div>
             <div>
-            <p>Resultados</p>
-            {content}
-            {isFetchingNextPage && <p className="center">Loading More Posts...</p>}
-            <p className="center"><a href="#top">Back to Top</a></p>
+                <p>Resultados</p>
+                {content}
+                {isFetchingNextPage && <p className="center">Loading More Posts...</p>}
+                <footer className="footer">
+                    <a>Sobre</a>
+                    <a>FAQ</a>
+                    <a>Termos de uso</a>
+                    <a>Politica de privacidade</a>
+                </footer>
             </div>
         </>
     )
